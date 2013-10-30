41037f6ac285b262d74e6e0bbc0bd691d6958d42
kittipong wannarat <kittipong.wannarat@gmail.com>
2013-10-30 23:07:56 +0700

    Complete PDOStatement

--- PDOStatement.php
+++ PDOStatement.php
@@ -1,3 +1,12 @@
 <?php
+$db_conn=new PDO('mysql:host=localhost;dbname=my-project-1','root','1234');
+//perform query
+$stmt=$db_conn->query('SELECT name,chef FROM recipes');
 
+//display results
+while($row=$stmt->fetch()) {
+
+echo $row['name'].' by '.$row['chef'] ."<br>"; 
+
+}
 ?>
